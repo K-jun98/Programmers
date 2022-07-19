d9eac9b26fd835b3ae11e41f054f909bb44f07fc
980327 <alap_u@naver.com>
2022-07-19 09:38:03 +0900

    월간 코드 챌린지 시즌1 - 두 개 뽑아서 더하기

---
+++ programmers/src/두 개 뽑아서 더하기.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+
+    public Integer[] solution(int[] numbers) {
+        Integer[] answer = {};
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < numbers.length-1; i++) {
+            for (int j = i+1; j < numbers.length; j++) {
+                set.add(numbers[i]+numbers[j]);
+            }
+        }
+        answer = set.toArray(new Integer[set.size()]);
+        Arrays.sort(answer);
+        return answer;
+    }
+}
