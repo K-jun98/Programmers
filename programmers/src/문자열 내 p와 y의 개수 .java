fbd13cd45cc8eb0d127aa259f299f36137dff18b
gobeomjun <alap_u@naver.com>
2022-09-19 16:40:55 +0900

    연습문제 - 문자열 내 p와 y의 개수

---
+++ programmers/src/문자열 내 p와 y의 개수 .java
@@ -0,0 +1,9 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        s=s.toLowerCase();
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
