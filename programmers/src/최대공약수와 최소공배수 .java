f26073bcf7ae57c738065edcd83465485e3ffb97
K-jun98 <alap_u@naver.com>
2022-09-28 14:22:09 +0900

    연습문제 - 최대공약수와 최소공배수

--- programmers/src/최대공약수와 최소공배수 .java
+++ programmers/src/최대공약수와 최소공배수 .java
@@ -1,17 +1,11 @@
 class Solution {
     public int[] solution(int n, int m) {
-        int[] answer = {gcd(n,m),lcm(n,m)};
+        int[] answer = {gcd(n, m), lcm(n, m)};
         return answer;
     }
 
-    static int gcd(int a, int b) { //최대 공약수
-        if (a < b) // 유클리드 호제법 조건
-        {
-            int temp = a;
-            a = b;
-            b = temp;
-        }
-        while (b != 0) { // 유클리드 호제법
+    static int gcd(int a, int b) {
+        while (b != 0) {
             int r = a % b;
             a = b;
             b = r;
@@ -19,7 +13,7 @@ class Solution {
         return a;
     }
 
-    static int lcm(int a, int b) { //최소 공배수
+    static int lcm(int a, int b) {
         return a * b / gcd(a, b);
     }
 }
