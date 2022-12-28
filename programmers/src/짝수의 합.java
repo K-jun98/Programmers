954424f57e27ae80ee8af4b8df136978e12f0d14
K-jun98 <alap_u@naver.com>
2022-12-28 23:03:32 +0900

    코딩테스트 입문 - 짝수의 합

---
+++ programmers/src/짝수의 합.java
@@ -0,0 +1,14 @@
+import static java.util.stream.Collectors.reducing;
+
+import java.util.stream.Stream;
+
+class Solution {
+
+    public int solution(int n) {
+        return Stream.iterate(0, i -> i + 1)
+                .limit(n + 1)
+                .filter(i -> i % 2 == 0)
+                .collect(reducing(Integer::sum))
+                .get();
+    }
+}
