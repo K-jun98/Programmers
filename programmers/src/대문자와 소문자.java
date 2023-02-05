661ff5dbfa3ce318e1ae887ebbedffbd589c1062
jun <alap_u@naver.com>
2023-02-05 23:37:21 +0900

    대문자와 소문자

---
+++ programmers/src/대문자와 소문자.java
@@ -0,0 +1,10 @@
+import java.util.stream.Collectors;
+
+class Solution {
+
+    public String solution(String myString) {
+        return myString.chars().mapToObj(operand -> String.valueOf(
+                (char) (Character.isLowerCase(operand) ? Character.toUpperCase(operand)
+                        : Character.toLowerCase(operand)))).collect(Collectors.joining());
+    }
+}
