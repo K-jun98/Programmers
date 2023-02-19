803b208775e1d302a96cbb17f5181e3c1dc71d76
jun <alap_u@naver.com>
2023-02-19 21:39:05 +0900

    n의 배수 고르기

--- programmers/src/n의 배수 고르기.java
+++ programmers/src/n의 배수 고르기.java
@@ -1,20 +1,8 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int n, int[] numlist) {
-
-        int[] arry = new int[numlist.length];
-        int count = 0;
-        for(int i = 0; i<numlist.length; i++){
-            if( numlist[i]%n == 0){
-                arry[count] = numlist[i];
-                count++;
-            }
-        }
-
-        int[] answer = new int[count];
-        for(int i = 0; i<count; i++){
-            answer[i] = arry[i];
-        }
-
-        return answer;
+        return Arrays.stream(numlist).filter(x -> x % n == 0)
+                .toArray();
     }
 }
