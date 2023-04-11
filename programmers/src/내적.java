8c2aac6742a60db9c78847de76311c7b1efc3dcb
jun <alap_u@naver.com>
2023-04-11 23:05:26 +0900

    내적.java

--- programmers/src/내적.java
+++ programmers/src/내적.java
@@ -4,9 +4,9 @@ public class Solution {
     }
 
     public int solution(int[] a, int[] b) {
-        int answer =0;
-        for(int i=0;i<a.length;i++){
-            answer+=(a[i]*b[i]);
+        int answer = 0;
+        for (int i = 0; i < a.length; i++) {
+            answer += (a[i] * b[i]);
         }
         return answer;
     }
