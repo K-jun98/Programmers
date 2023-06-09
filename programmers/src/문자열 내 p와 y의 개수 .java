daa968bf635a11721dd4c7d4be25055be5af7d25
jun <alap_u@naver.com>
2023-06-09 23:55:12 +0900

    문자열 내 p와 y의 개수 .java

--- programmers/src/문자열 내 p와 y의 개수 .java
+++ programmers/src/문자열 내 p와 y의 개수 .java
@@ -1,17 +1,18 @@
 class Solution {
     boolean solution(String s) {
-        s=s.toLowerCase();
+        s = s.toLowerCase();
         int value = 0;
-        for(int i=0; i < s.length();i++){
-            if(s.charAt(i)=='p'){
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'p') {
                 value++;
-            }else if(s.charAt(i)=='y'){
+            } else if (s.charAt(i) == 'y') {
                 value--;
             }
         }
-        if(value==0)
+        if (value == 0) {
             return true;
-        else
+        } else {
             return false;
+        }
     }
 }
