9e6ef2b07d3ea4f6bf4b854d98da723fe2482ff6
jun <alap_u@naver.com>
2023-07-09 21:03:42 +0900

    문자열 다루기 기본 .java

--- programmers/src/문자열 다루기 기본 .java
+++ programmers/src/문자열 다루기 기본 .java
@@ -1,19 +1,19 @@
-    class Solution {
-        public boolean solution(String s) {
-            if (s.length() == 4 || s.length() == 6) {
-                if (judge(s)) {
-                    return true;
-                }
+class Solution {
+    public boolean solution(String s) {
+        if (s.length() == 4 || s.length() == 6) {
+            if (judge(s)) {
+                return true;
             }
-            return false;
         }
-
-        public static boolean judge(String s) {
-            for (char c : s.toCharArray()) {
-                if (c < '0' || c > '9') {
-                    return false;
-                }
-            }
-            return true;
-        }
+        return false;
     }
+
+    public static boolean judge(String s) {
+        for (char c : s.toCharArray()) {
+            if (c < '0' || c > '9') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
