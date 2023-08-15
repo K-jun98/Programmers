1701b6d101acc18ccd7d8003b067faf07d4cb5f8
jun <alap_u@naver.com>
2023-08-15 23:30:02 +0900

    세균 증식.java

--- programmers/src/세균 증식.java
+++ programmers/src/세균 증식.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int n, int t) {
         int answer = n;
         while (t != 0) {
-            answer = answer*2;
+            answer = answer * 2;
             t--;
         }
         return answer;
