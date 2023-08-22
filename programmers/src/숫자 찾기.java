ffe2612de37d97b06b5c55c7584407fe991081b8
jun <alap_u@naver.com>
2023-08-22 22:38:23 +0900

    숫자 찾기.java

--- programmers/src/숫자 찾기.java
+++ programmers/src/숫자 찾기.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int num, int k) {
-        String strNum = num+"";
-        String strK = k+"";
+        String strNum = num + "";
+        String strK = k + "";
 
         int answer = strNum.indexOf(strK);
 
-        return answer>-1 ? ++answer : answer;
+        return answer > -1 ? ++answer : answer;
     }
 }
